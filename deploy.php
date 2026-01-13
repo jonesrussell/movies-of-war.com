6100576e99c7e8b853ca0eab5a1fbb9864efddf1
Russell Jones <jonesrussell42@gmail.com>
2026-01-13 02:12:29 -0500

    Update deployment configuration in deploy.php
    - Added shared files including '.env' and 'database/database.sqlite' to the deployment setup, ensuring necessary configurations are available during deployment.
    - Defined writable directories for 'storage', 'bootstrap/cache', and 'database', enhancing the deployment process by specifying directories that require write permissions.

--- deploy.php
+++ deploy.php
@@ -8,9 +8,16 @@ require 'recipe/laravel.php';
 set('repository', 'git@github.com:jonesrussell/movies-of-war.com.git');
 set('git_ssh_command', 'ssh -i ~/.ssh/id_ed25519_github -o IdentitiesOnly=yes');
 
-add('shared_files', []);
+add('shared_files', [
+    '.env',
+    'database/database.sqlite',
+]);
 add('shared_dirs', []);
-add('writable_dirs', []);
+add('writable_dirs', [
+    'storage',
+    'bootstrap/cache',
+    'database',
+]);
 
 // Hosts
 

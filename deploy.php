93439cbb3e58080f980ba58e8ae50e9c66e57f9b
Russell Jones <jonesrussell42@gmail.com>
2026-01-13 02:02:36 -0500

    Add SSH command configuration for deployment in deploy.php
    - Updated the `deploy.php` file to include a specific SSH command for Git operations, enhancing security and ensuring the correct identity is used during deployment.
    - This change improves the deployment process for the Movies of War project by specifying the SSH key to be used.

--- deploy.php
+++ deploy.php
@@ -6,6 +6,7 @@ require 'recipe/laravel.php';
 // Config
 
 set('repository', 'git@github.com:jonesrussell/movies-of-war.com.git');
+set('git_ssh_command', 'ssh -i ~/.ssh/id_ed25519_github -o IdentitiesOnly=yes');
 
 add('shared_files', []);
 add('shared_dirs', []);

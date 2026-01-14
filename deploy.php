109a36a79a4328601626e913becd54465a2452dc
Russell Jones <jonesrussell42@gmail.com>
2026-01-14 12:51:31 -0500

    Update SSH command path in deploy.php for consistency with deployment environment

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 // Config
 
 set('repository', 'git@github.com:jonesrussell/movies-of-war.com.git');
-set('git_ssh_command', 'ssh -i ~/.ssh/id_ed25519_github -o IdentitiesOnly=yes');
+set('git_ssh_command', 'ssh -i /home/deployer/.ssh/id_ed25519_github -o IdentitiesOnly=yes');
 
 add('shared_files', [
     '.env',

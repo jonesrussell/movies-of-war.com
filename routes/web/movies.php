40df419b516f5ace284184240ebeca3b75095e0d
Russell Jones <jonesrussell42@gmail.com>
2026-02-01 20:29:52 -0500

    Add reviews routing and admin management functionality
    - Integrated reviews routing into the application, allowing users to view, create, update, and delete reviews.
    - Added admin routes for managing reviews, including moderation and publishing controls.
    - Established a new reviews.php file to handle review-related routes and actions.
    - Updated existing movie routes to include review functionalities, enhancing user interaction with movie reviews.

--- routes/web/movies.php
+++ routes/web/movies.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie:slug}/reviews', [ReviewController::class, 'index'])->name('movies.reviews.index');
 Route::get('/movies/{slug}', [MovieController::class, 'show'])->name('movies.show');
+Route::get('/reviews/{review}', [ReviewController::class, 'show'])->name('reviews.show');
